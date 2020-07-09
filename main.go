86bf2b1ea6ba404bf85ed93e2e1cb6b2c06880b0
EC2 Default User <ec2-user@ip-10-139-8-30.ap-northeast-1.compute.internal>
2020-07-09 11:49:18 +0000

    update 20200709

--- main.go
+++ main.go
@@ -3,39 +3,67 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
+	"os"
 	"strings"
 )
 
-var (
-	masterdata = `{ "Hello": "world" }`
-)
-
 type Message struct {
-	Hello string
+	Path string `json:"path"`
+	Body string `json:"body"`
 }
 
 func main() {
-	fmt.Println("start server")
+	fmt.Println("====== read masterdata =====")
 
-	decoder := json.NewDecoder(strings.NewReader(masterdata))
-
-	var message Message
-	err := decoder.Decode(&message)
+	fp, err := os.Open("messages.json")
 	if err != nil {
 		fmt.Printf("%s", err)
 	}
-	fmt.Printf("%v", message.Hello)
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(200)
-		w.Write([]byte("Hello browser"))
-	})
+	defer fp.Close()
 
-	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(200)
-		w.Write([]byte("world"))
-	})
+	bytes := make([]byte, 256)
+	stringBytes := make([]byte, 0)
 
-	http.ListenAndServe("localhost:11181", nil)
+	for {
+		index, err := fp.Read(bytes)
+		if err == io.EOF {
+			fmt.Println("EOF")
+			break
+		}
+		if err != nil {
+			fmt.Printf("%s", err)
+			break
+		}
+		fmt.Printf("%d", index)
+		fmt.Printf("%v", string(bytes))
+
+		stringBytes = append(stringBytes, bytes...)
+	}
+	fmt.Println("===== start server =====")
+
+	decoder := json.NewDecoder(strings.NewReader(string(stringBytes)))
+
+	var messages []Message
+	err = decoder.Decode(&messages)
+	if err != nil {
+		fmt.Printf("%s", err)
+	}
+	for _, m := range messages {
+		fmt.Println(m.Path)
+
+		path := m.Path
+		body := m.Body
+		http.HandleFunc(path, func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(200)
+			w.Write([]byte(body))
+		})
+	}
+
+	err = http.ListenAndServe(":80", nil)
+	if err != nil {
+		fmt.Printf("%s", err)
+	}
 }
